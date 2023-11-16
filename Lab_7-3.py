533d396a527885da1e9319838341aa31c1c7c291
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-11-16 11:18:38 -0500

    finished

---
+++ Lab_7-3.py
@@ -0,0 +1,20 @@
+
+#Create a Python file named lab_7-3.py
+
+#*** You must write a comment for every chunk of code you write from now on along with your author tag***
+
+#Copy your lab 7-1 code into the file
+
+def find_sum(num1,num2):
+    num_sum =num1 + num2
+    return num_sum
+
+print(find_sum(111,222))
+print(find_sum(1,2) == 3)
+print (find_sum(1,4) == 5)
+print (find_sum(220,220) == 440)
+
+#Add 4 test cases to the end of the file, with comments
+#Ensure your lab runs accurately
+
+
